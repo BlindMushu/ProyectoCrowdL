8240a4ae676851baeb0b1d213816ed2cdab02243
Daniel Quispe <daniel20111quispe@gmail.com>
2019-05-16 17:40:40 -0400

    Tercer commit de prueba de creacion de controller

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -4,6 +4,6 @@
 @section('title','Home')
 
 @section('content')
-    <h1>Hola esto es Bootstrap</h1>
+    <h1>Hola esto es Bootstrap </h1>
     <a href="" class="btn btn-success">Boton</a>
 @endsection
